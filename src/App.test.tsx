62c1ee79fd0397ffcbdb84ac4139248eaf9d1925
L1senochek <lisenochek96@gmail.com>
2024-07-08 20:32:59 +0300

    feat: add setup tests & coverage / update .gitignore

---
+++ src/App.test.tsx
@@ -0,0 +1,26 @@
+import { act, render } from '@testing-library/react';
+import { describe, expect, it, test } from 'vitest';
+import App from './App';
+import { ReactElement } from 'react';
+
+describe('App: ', (): void => {
+  it('Renders headline.', async () => {
+    await act(async (): Promise<void> => {
+      render(<App />);
+      await Promise.resolve();
+    });
+  });
+});
+
+test('Initial test. Adds 1 + 2 to equal 3.', (): void => {
+  const a = 1;
+  const b = 2;
+  expect(a + b).toBe(3);
+});
+
+it('Testing ReactElement.', (): void => {
+  const App = (): ReactElement => {
+    return <h1>Test h1</h1>;
+  };
+  expect(<App />).not.toBeNull();
+});
