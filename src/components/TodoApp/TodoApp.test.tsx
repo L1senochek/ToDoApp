1b25d0065695928b2a529a36a94a8160a322b877
l1senochek <lisenochek96@gmail.com>
2024-07-09 10:09:14 +0300

    feat: update TodoApp tests

--- src/components/TodoApp/TodoApp.test.tsx
+++ src/components/TodoApp/TodoApp.test.tsx
@@ -1,25 +1,41 @@
 import { render, screen, fireEvent } from '@testing-library/react';
 import TodoApp from './TodoApp';
-import { describe, expect, it } from 'vitest';
+import { beforeEach, describe, expect, it } from 'vitest';
 
 describe('TodoApp: ', (): void => {
-  it('Renders initial todos.', (): void => {
+  beforeEach(() => {
     render(<TodoApp />);
+  });
+
+  it('Renders initial todos.', (): void => {
     expect(screen.getByText(/Тестовое задание/i)).toBeTruthy();
     expect(screen.getByText(/Прекрасный код/i)).toBeTruthy();
     expect(screen.getByText(/Покрытие тестами/i)).toBeTruthy();
   });
 
   it('Adds a new todo.', (): void => {
-    render(<TodoApp />);
     const input = screen.getByPlaceholderText(/What needs to be done?/i);
     fireEvent.change(input, { target: { value: 'New Todo' } });
     fireEvent.submit(input);
     expect(screen.getByText(/New Todo/i)).toBeTruthy();
   });
 
+  it('Should filter todos.', (): void => {
+    const allButton = screen.getByText('All');
+    const activeButton = screen.getByText('Active');
+    const completedButton = screen.getByText('Completed');
+
+    fireEvent.click(activeButton);
+    expect(screen.queryByText(/Прекрасный код/i)).not.toBeTruthy();
+
+    fireEvent.click(completedButton);
+    expect(screen.queryByText('Прекрасный код')).toBeTruthy();
+
+    fireEvent.click(allButton);
+    expect(screen.getByText('Тестовое задание')).toBeTruthy();
+  });
+
   it('Clears completed todos.', (): void => {
-    render(<TodoApp />);
     const clearButton = screen.getByText(/Clear Completed/i);
     fireEvent.click(clearButton);
     expect(screen.queryByText(/Прекрасный код/i)).toBeNull();
